76fbfed2a1976f7a5cd5e7efd1babf64f766bd8d
Ricardo Longa <ricardo.longa@neoway.com.br>
2015-02-24 20:05:09 -0300

    first commit

---
+++ src/br/com/ricardolonga/Aplicacao.java
@@ -0,0 +1,13 @@
+package br.com.ricardolonga;
+
+/**
+ * Observe a complexidade de adicionar o evento "cancelar" e o status CANCELADO.
+ * Estamos violando o princípio OCP pois para estender o sistema é necessário alterar as classes já prontas.
+ */
+public class Aplicacao {
+
+    public static void main(String[] args) {
+
+    }
+
+}
